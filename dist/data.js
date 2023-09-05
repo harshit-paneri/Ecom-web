84c2ae47326a3ccb1a88f7853ac803130db159e6
Harshit <harshitpanari@gmail.com>
2023-09-05 15:26:07 +0530

    e com web

---
+++ dist/data.js
@@ -0,0 +1,21 @@
+const data = [{
+    imgage : "https://3635909.app.netsuite.com/core/media/media.nl?id=3966235&c=3635909&h=o-y7dRBz6-eubhNVD90wJZNIZXyDQ1zikn9ryZSLUJ8fW4lY&_xt=.bin",
+    title : "Gait Trainers & Walkers",
+    price : "$600.00",
+},
+{
+    imgage : "https://3635909.app.netsuite.com/core/media/media.nl?id=3966236&c=3635909&h=gtX6XelgoJc62lhxsIVlOxFsXNvPYyxJ8R2HPIIZ9nBp7qZz&_xt=.bin",
+    title : "Manual Stand-up Lifts",
+    price : "$400.00",
+},
+{
+    imgage : "https://3635909.app.netsuite.com/core/media/media.nl?id=3847481&c=3635909&h=lsUT05Uivuu6QPLwLIsnI5MaS_ripyilGkViJpm-rseyN2g8&_xt=.bin",
+    title : "Asthma Care Supplies",
+    price : "$2.00",
+},
+{
+    imgage : "https://3635909.app.netsuite.com/core/media/media.nl?id=3966125&c=3635909&h=W2n2J-DwRdVeS-rlJt94hJJHC9N8laAaaUwNgghvq2jBKYDQ&_xt=.bin",
+    title : "Wound Care Dressings",
+    price : "$1.00",
+}
+]
